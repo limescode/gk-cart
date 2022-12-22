1fd0511a8874350ad1b9d5f091a3450ae049fca9
limescode <katechizis@gmail.com>
2022-12-22 21:49:49 +0100

    git initial commit

---
+++ src/main/java/pl/limescode/gkcart/GkCartApplication.java
@@ -0,0 +1,13 @@
+package pl.limescode.gkcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GkCartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/limescode/gkcart/GkCartApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.limescode.gkcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
